2c721f66f6eb80fecdd3a8c69207a3a3851327ac
23080ad <23080ad@hvhs.school.nz>
2026-06-15 11:05:27 +1200

    working on window.parent

--- Geodash/Geodash.js
+++ Geodash/Geodash.js
@@ -16,6 +16,10 @@ function endGame(_player, _obstacle){
     player.remove();
     obstacles.removeAll();
     // Put your database writes here:
+  function writeScoreToDB(score) {
+    console.log("test score: " + score)
+    notifyParent();
+}
 
 
 
--- fb_io.js
+++ fb_io.js
@@ -1,8 +1,13 @@
 /***************************************************************/
 //VARIABLES
 /***************************************************************/
+//Users account variables
 var uid;
 var userEmail;
+var authenticationListener;
+
+//score managing variables
+var userScore = 5; //this is a test value for now
 /***************************************************************/
 
 //checks that all user data has been filled out before allowing login
@@ -11,13 +16,24 @@ function checkLoginData() {
     userName = document.getElementById("userName").value;
     if (userName == null || userAge == null) {
         console.log("you must fillout all fields");
-    } else { 
+    } else {
         console.log(userAge);
         console.log(userName);
-        fb_authenticate(); 
+        fb_authenticate();
     }
 
 }
+
+//doesnt work as it says
+// function fb_login() {
+//     authenticationListener = firebase.auth().onAuthStateChanged(handleLogin);
+// }
+
+// function fb_logOut() {
+//     authenticationListener();
+//     firebase.auth.signOut();
+//     console.log("logged out");
+// }
 
 function fb_authenticate() {
     console.log("Logging in")
@@ -44,9 +60,30 @@ function fb_authenticate() {
                 var user = result.user;
             });
         }
+        //fill in the table with the users details
+        document.getElementById("userAge_id").innerHTML = userAge;
+        document.getElementById("userName_id").innerHTML = userName;
+        document.getElementById("userEmail_id").innerHTML = userEmail;
+
+        addUserToDatabase();
     });
-    //fill in the table with teh users details
-    document.getElementById("userAge_id").innerHTML = userAge;
-    document.getElementById("userName_id").innerHTML = userName;
-    document.getElementById("userEmail_id").innerHTML = userEmail;
+}
+
+//add user to the main databse when they login for the first time
+function addUserToDatabase() {
+    firebase.database().ref('/test_userInfo/players/' + uid + "/").set({
+        Username: userName,
+        Age: userAge,
+        Email: userEmail,
+        Score_game_1: userScore,
+        Score_game_2: userScore,
+    });
+
+}
+
+
+function notifyParent() {
+    window.parent.writeScoreToDB("Hello!");
+
+
 }
